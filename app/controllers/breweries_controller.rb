8268210a07899ee8b27e65eb32782e8fdae44d26
Simo Makinen <simo.makinen@cs.helsinki.fi>
2014-01-19 21:02:20 +0200

    Beer view now has additional links and links are names instead of ids. Finishing touches for first week exercises

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,7 +4,7 @@ class BreweriesController < ApplicationController
   # GET /breweries
   # GET /breweries.json
   def index
-    @breweries = Brewery.all
+    @breweries = Brewery.all  
   end
 
   # GET /breweries/1
