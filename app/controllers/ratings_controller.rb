f9b6af246b14063bd1f2923c8b730dfe05d70547
Simo Makinen <simo.makinen@cs.helsinki.fi>
2014-01-26 14:41:56 +0200

    Ratings can be created with a form. Beer shows ratings average.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,15 @@
 class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
+  end
 
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    Rating.create params.require(:rating).permit(:score, :beer_id)
+    redirect_to ratings_path
   end
 
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,20 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    times_rated = ratings.count
+
+    # init sum to float for avg calculation
+    ratings_sum = ratings.inject(0.0) { |sum, rating |
+      sum + rating.score
+    }
+
+    # calc avg if there are ratings, default to 0
+    avg_score = (times_rated > 0) ? ratings_sum / times_rated : 0
+
+    # alternative method with AR
+    #ratings.average('score').to_s
+
+  end
 end
